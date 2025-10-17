a897acf36759fe935c6b619242705a486ef22535
TurnerHelical <hunterde91@gmail.com>
2025-10-17 14:22:28 -0400

    refactored to use express

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-const http = require('http');
 const path = require('path');
-const fs = require('fs');
+const express = require('express');
 
+const app = express();
 const port = 8080;
 
-const server = http.createServer((req,res) => {
-    res.setHeader('Content-Type', 'text/html')
-
-    let filepath = '';
-
-    if (req.url === '/' && req.method === 'GET' ) {
-        filepath = path.join(__dirname,'index.html');
-    } else if (req.url === '/about' && req.method === 'GET') {
-        filepath = path.join(__dirname, 'about.html');
-    } else if (req.url === '/contact' && req.method === 'GET') {
-        filepath = path.join(__dirname,'contactMe.html');
-    } else {
-        filepath= path.join(__dirname,'404.html');
-        res.statusCode = 404;
-    }
-
-    fs.readFile(filepath, (err,data) => {
-        if (err) {
-            res.statusCode = 500;
-            res.end('<h1>500 - Internal Server Error <h1>')
-        } else {
-            res.statusCode = res.statusCode || 200;
-            res.end(data);
-        }
-    });
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 
-server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-})
+app.get('/about', (req,res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/contact', (req,res) => {
+    res.sendFile(path.join(__dirname,'contactMe.html'));
+});
+
+app.use((req,res) => {
+    res.status(404).sendFile(path.join(__dirname, '404.html'));
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('<h1>500 - Internal Server Error </h>');
+});
+
+app.listen(port,() => {
+
+    console.log(`Server running at http://localhost:${port}`);
+});
